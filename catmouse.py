da6bb81f83556b77ddc58461f3c7e60185d73142
Massimo <massimopredosin@gmail.com>
2017-11-25 11:20:53 -0800

    Added main method Increased FPS to 60 Added DEBUG flag to toggle print statements

--- catmouse.py
+++ catmouse.py
@@ -2,40 +2,26 @@
 import pygame, sys, random
 from pygame.locals import *
 
+# GLOBAL VARIABLES
+DEBUG = False
+FPS = 60 # frames per second setting
+screen_width=1200
+screen_height=1000
+
 class Direction:
  N,NE,E,SE,S,SW,W,NW = range(1,9)
 
-pygame.init()
-
-FPS = 30 # frames per second setting
-fpsClock = pygame.time.Clock()
-
-# set up the window
-screen_width=1200
-screen_height=1000
-screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
-pygame.display.set_caption('Animation')
-
-points = 0
-
-WHITE = (255, 255, 255)
-BLUE = (33, 135, 203)
-SILVER = (192, 192, 192)
-TEAL = (0, 128, 128)
-ORANGE = (225, 102, 31)
-
-random.seed()
-
 class Sprite:
  image_path = 'cat.png'
- speed=10
+ speed=10*30/FPS
  x = 0
  y = 0
 
  def __init__(self):
   self.image = pygame.image.load(self.image_path)
   self.rect = pygame.Rect(0, 0, self.image.get_width(), self.image.get_height())
-  print 'rect = {}'.format(self.rect)
+  if DEBUG:
+   print 'rect = {}'.format(self.rect)
  
  def movex(self, x):
   self.x += x
@@ -75,7 +61,7 @@ class Mouse(Sprite):
  image_path = 'mouse.png'
  direction = Direction.E
  counter = 0
- speed = 15
+ speed = 15*30/FPS
  
  def __init__(self):
   Sprite.__init__(self)
@@ -100,9 +86,10 @@ class Mouse(Sprite):
 
  def animate(self):
   if self.counter == 0:
-   self.counter = random.randint(20,30)
+   self.counter = random.randint(30,80)
    self.direction = random.randint(1,8)
-   print 'counter = {} direction = {}'.format(self.counter, self.direction)
+   if DEBUG:
+    print 'counter = {} direction = {}'.format(self.counter, self.direction)
 
   self.counter -= 1
 
@@ -127,44 +114,69 @@ class Mouse(Sprite):
    self.movex(-self.speed*2/3)
    self.movey(-self.speed*2/3)
 
-cat = Sprite()
-mouse = Mouse()
+def main():
 
-pygame.key.set_repeat(True)
+ pygame.init()
 
-# MAIN GAME LOOP
-while True:
+ clock = pygame.time.Clock()
 
-# 1. GET DATA
- for event in pygame.event.get():
-  if event.type == QUIT:
-   pygame.quit()
-   sys.exit()
-  elif event.type == pygame.KEYDOWN:
-   if event.key == pygame.K_LEFT:
-    cat.movex(-cat.speed)
-   if event.key == pygame.K_RIGHT:
-    cat.movex(cat.speed)
-   if event.key == pygame.K_UP:
-    cat.movey(-cat.speed)
-   if event.key == pygame.K_DOWN:
-    cat.movey(cat.speed)
+ # set up the window
+ screen = pygame.display.set_mode([screen_width, screen_height])
 
-# 2. PROCESS DATA
- mouse.animate()
+ points = 0
 
- if cat.collided(mouse):
-   points += 1
-   mouse.reset()
+ WHITE = (255, 255, 255)
+ BLUE = (33, 135, 203)
+ SILVER = (192, 192, 192)
+ TEAL = (0, 128, 128)
+ ORANGE = (225, 102, 31)
 
- print 'cat = {},{} mouse = {},{} points = {}'.format(cat.x, cat.y, mouse.x, mouse.y, points)
+ random.seed()
 
-# 3. OUTPUT DATA
- screen.fill(TEAL)
- pygame.draw.circle(screen, ORANGE, (screen_width/2, screen_height/2), 30, 0)
- mouse.blit(screen)
- cat.blit(screen)
+ cat = Sprite()
+ mouse = Mouse()
 
- pygame.display.update()
- fpsClock.tick(FPS)
+ pygame.key.set_repeat(True)
+
+ # MAIN GAME LOOP
+ while True:
+
+ # 1. GET DATA
+  for event in pygame.event.get():
+   if event.type == QUIT:
+    pygame.quit()
+    sys.exit()
+   elif event.type == pygame.KEYDOWN:
+    if event.key == pygame.K_LEFT:
+     cat.movex(-cat.speed)
+    if event.key == pygame.K_RIGHT:
+     cat.movex(cat.speed)
+    if event.key == pygame.K_UP:
+     cat.movey(-cat.speed)
+    if event.key == pygame.K_DOWN:
+     cat.movey(cat.speed)
+    if event.key == pygame.K_ESCAPE:
+     pygame.quit()
+
+  # 2. PROCESS DATA
+  mouse.animate()
+
+  if cat.collided(mouse):
+    points += 1
+    mouse.reset()
+
+  if DEBUG:
+   print 'cat = {},{} mouse = {},{} points = {}'.format(cat.x, cat.y, mouse.x, mouse.y, points)
+
+  # 3. OUTPUT DATA
+  screen.fill(TEAL)
+  pygame.draw.circle(screen, ORANGE, (screen_width/2, screen_height/2), 30, 0)
+  mouse.blit(screen)
+  cat.blit(screen)
+
+  pygame.display.flip()
+  clock.tick(FPS)
+  pygame.display.set_caption("fps: " + str(round(clock.get_fps(),1))+" points: " + str(points))
+
+main()
 
