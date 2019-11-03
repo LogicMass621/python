dcf2edf9495b72cf9afeb85ed04752c3adff8b3c
LogicMass621 <massimopredosin@gmail.com>
2019-11-03 13:00:41 -0800

    added argument to Counter.py

--- Counter.py
+++ Counter.py
@@ -1,5 +1,17 @@
-counter = input('What number do you want me to count up to?')
-x = 0
-while x < int(counter):
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) > 1:
+    counter = sys.argv[1]
+else:
+    counter = input('Please enter a number:')
+
+try:
+    counter = int(counter)
+except ValueError:
+    print("Invalid argument: " + counter)
+    
+x = 1
+while x <= counter:
     print(x)
     x = x+1
