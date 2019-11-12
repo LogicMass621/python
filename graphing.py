ae39a2cc1032e7a7fe8f3d3c13934a617e426852
LogicMass621 <massimopredosin@gmail.com>
2019-11-11 19:08:12 -0800

    cleanup graphing.py

--- graphing.py
+++ graphing.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python
 
-import pygame, sys
-from pygame.locals import *
-
-pygame.init()
+import pygame
 
 black = [0, 0, 0]
 white = [255, 255, 255]
-green = [0,255,0]
-blue = [0,0,255]
-X=20
-Y=380
-X2 = 380
-Y2 = 20
 
+screenWidth = 400
+screenHeight = 400
+
+pygame.init()
 pygame.font.init()
-display_surface = pygame.display.set_mode([X,Y])
-font = pygame.font.Font('freesansbold.ttf', 20)
-text = font.render('X', True, black, white)
-textRect = text.get_rect()
-textRect.center = (X // 2, Y // 2)
-
-display_surface2 = pygame.display.set_mode([X2,Y2])
-font = pygame.font.Font('freesansbold.ttf', 20)
-text2 = font.render('Y', True, black, white)
-textRect2 = text2.get_rect()
-textRect2.center = (X2 // 2, Y2 // 2)
-
-screen = pygame.display.set_mode([400,400])
 pygame.display.set_caption('Graph')
-screen.fill(white)
 
-pygame.display.flip()
-display_surface.blit(text, textRect)
-display_surface2.blit(text2, textRect2)
+font = pygame.font.Font('freesansbold.ttf', 20)
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 
-class Graph:
-    pygame.draw.line(screen,black, [200,0], [200,400],1)
-    pygame.draw.line(screen,black,[0,200],[400,200],1)
-    x = 200
-    y=200
+textX = font.render('X', True, black, white)
+textRectX = textX.get_rect()
+textY = font.render('Y', True, black, white)
+textRectY = textY.get_rect()
 
-pygame.display.flip()
+def render():
+
+    screenWidth = screen.get_width()
+    screenHeight = screen.get_height()
+
+    # clear screen to white
+    screen.fill(white)
+
+    textRectX.y = screenHeight/2 - textRectX.height
+    textRectY.x = screenWidth/2 - textRectY.width
+
+    screen.blit(textX, textRectX)
+    screen.blit(textY, textRectY)
+
+    pygame.draw.line(screen,black, [screenWidth/2,0], [screenWidth/2,screenHeight],1)
+    pygame.draw.line(screen,black, [0,screenHeight/2],[screenWidth,screenHeight/2],1)
+
+    pygame.display.update()
+
 running = True
 while running:
-  for event in pygame.event.get():
+    event = pygame.event.wait();
+    #print(event)
     if event.type == pygame.QUIT:
-      running = False
-      
+        running = False
+
+    render()
+
