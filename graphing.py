61fe41aaa8a1ed5ef586630a92975afb4de970ce
LogicMass621 <massimopredosin@gmail.com>
2019-11-23 14:45:36 -0800

    Add line plot

--- graphing.py
+++ graphing.py
@@ -5,8 +5,19 @@ import pygame
 black = [0, 0, 0]
 white = [255, 255, 255]
 
-screenWidth = 400
-screenHeight = 400
+screenWidth = 800
+screenHeight = 800
+
+Min = -screenWidth/2#float(input('What is the lower number in the domain:'))
+Max = screenWidth/2#float(input('What is the higher number in the domain:'))
+interval = 1#float(input('Enter the interval you want to sample:'))
+b = 0#input('Enter the constant in the function:')
+m = 1#input('Enter the coefficent of x:')
+
+def tran(x,y):
+    x = screenWidth/2 + x
+    y = screenHeight/2 - y
+    return x,y
 
 pygame.init()
 pygame.font.init()
@@ -20,24 +31,34 @@ textRectX = textX.get_rect()
 textY = font.render('Y', True, black, white)
 textRectY = textY.get_rect()
 
+
+
 def render():
 
     screenWidth = screen.get_width()
-    screenHeight = screen.get_height()
+    screenHeight = screen.get_height()  
 
-    # clear screen to white
-    screen.fill(white)
+    # x label
+    screen.blit(textX, tran(screenHeight/2 - textRectX.width,0))
+    # y label
+    screen.blit(textY, tran(0,screenWidth/2))
 
-    textRectX.y = screenHeight/2 - textRectX.height
-    textRectY.x = screenWidth/2 - textRectY.width
+    # x-axis
+    pygame.draw.line(screen,black, tran(-screenWidth/2,0), tran(screenWidth/2,0),1)
+    # y-axis
+    pygame.draw.line(screen,black, tran(0,screenHeight/2),tran(0,-screenHeight/2),1)
 
-    screen.blit(textX, textRectX)
-    screen.blit(textY, textRectY)
-
-    pygame.draw.line(screen,black, [screenWidth/2,0], [screenWidth/2,screenHeight],1)
-    pygame.draw.line(screen,black, [0,screenHeight/2],[screenWidth,screenHeight/2],1)
-
-    pygame.display.update()
+    x = Min
+    y = m*x + b
+    x2 = x
+    y2 =y
+        
+    while x < Max:
+        y = m*x + b
+        pygame.draw.line(screen,black, tran(x2,y2) , tran(x,y) ,1)
+        x2 = x
+        y2 =y
+        x = x + interval
 
 running = True
 while running:
@@ -46,5 +67,7 @@ while running:
     if event.type == pygame.QUIT:
         running = False
 
+    # clear screen to white
+    screen.fill(white)
     render()
-
+    pygame.display.update()
