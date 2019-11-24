0712f691f96fca4f7a24125247795126bede2eda
LogicMass621 <massimopredosin@gmail.com>
2019-11-24 11:33:02 -0800

    Added translation

--- graphing.py
+++ graphing.py
@@ -8,21 +8,34 @@ white = [255, 255, 255]
 screenWidth = 800
 screenHeight = 800
 
-graphWidth = 20
-graphHeight = 20
+graphWidth = 100
+graphHeight = 100
+
+graphCenterX = 0
+graphCenterY = 0
 
 sx= screenWidth / float(graphWidth)
 sy= screenHeight / float(graphHeight)
 
-Min = -graphWidth / 2
-Max = graphWidth / 2
 interval = 1 / sx
 
+MinX = -graphWidth / 2 + graphCenterX
+MaxX = graphWidth / 2 + graphCenterX
+MinY = -graphHeight/2 + graphCenterY
+MaxY = graphHeight/2 + graphCenterY
+
+print("MinX",MinX,"MaxX",MaxX)
+print("MinY",MinY,"MaxY",MaxY)
+
 def tran(x,y):
+
+    # translate
+    x = x - graphCenterX
+    y = y - graphCenterY
     
     # scale
-    x=x*sx
-    y=y*sy
+    x = x * sx
+    y = y * sy
 
     # transform to screen coordinates
     x = screenWidth/2 + x
@@ -43,26 +56,26 @@ textY = font.render('Y', True, black, white)
 textXWidth = textX.get_width() / sx
 
 def compute_y(x):
-    return x*x
+    return x*x-24*x+144
     
 def render():
 
     # x label
-    screen.blit(textX, tran(graphHeight/2 - textXWidth,0))
+    screen.blit(textX, tran(MaxX - textXWidth,0))
     # y label
-    screen.blit(textY, tran(0,graphWidth/2))
+    screen.blit(textY, tran(0,MaxY))
 
     # x-axis
-    pygame.draw.line(screen,black, tran(-graphWidth/2,0), tran(graphWidth/2,0),1)
+    pygame.draw.line(screen,black, tran(MinX,0), tran(MaxX,0),1)
     # y-axis
-    pygame.draw.line(screen,black, tran(0,graphHeight/2),tran(0,-graphHeight/2),1)
+    pygame.draw.line(screen,black, tran(0,MinY),tran(0,MaxY),1)
 
-    x = Min
+    x = MinX
     y = compute_y(x)
     x2 = x
     y2 = y
         
-    while x < Max:
+    while x < MaxX:
         y = compute_y(x)
         pygame.draw.line(screen,black, tran(x2,y2) , tran(x,y) ,1)
         x2 = x
