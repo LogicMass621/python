e6a7b7d45f4f205b7dc441510caa402d9fe494f7
LogicMass621 <massimopredosin@gmail.com>
2020-03-17 17:19:20 -0700

    Added screen classes

--- breakout.py
+++ breakout.py
@@ -6,8 +6,8 @@ import sys
 class Rect:
     
     def __init__(self, x:float, y:float, width:int, height:int):
-        self.__x = float(x)
-        self.__y = float(y)
+        self.__x = x
+        self.__y = y
         self.__width = width
         self.__height = height
         self.__pygRect = pygame.Rect(self.__x,self.__y,self.__width,self.__height)
@@ -59,8 +59,17 @@ class Rect:
 
     def toPygame(self):
         return self.__pygRect
-    
+
+class Screen:
+
+    def render(self):
+        return
+
+    def handleEvent(self,event):
+        return
+
 pygame.init()
+pygame.font.init()
 
 print ('PyGame Version',pygame.version.ver)
 print ('SDL Version', pygame.get_sdl_version())
@@ -68,106 +77,177 @@ print ('SDL Version', pygame.get_sdl_version())
 screenWidth = 800
 screenHeight = 800
 
-black = (0,0,0)
 white = (255,255,255)
+black = (0,0,0)
 blue = (0,0,255)
-red = (255,0,0)
+red = (255,7,58)
+green = (50,255,50)
+teal =(0,255,255)
 
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption('Breakout!')
 
-paddleRectWidth = screenWidth/10
-paddleRect = Rect(screenWidth*0.5-paddleRectWidth*0.5,screenHeight*0.9,paddleRectWidth,screenHeight/75)
-paddleMoveSpeed = paddleRectWidth/4
+surface = pygame.display.set_mode((screenWidth, screenHeight))
 
-ballSize = 20
-ballRect = Rect((screenWidth-ballSize)/2,screenHeight/2,ballSize,ballSize)
+screens = []
 
-ystep = 3
-xstep = 0
-stepStrength = 20.0
-
-points = 0
-blocks = []
-i = 30
-
-while i<screenWidth-30:
-    blocks.append(Rect(i,80,20,20))
-    i += 50
-
-def render():
-    global ballRect
-    global xstep
-    global ystep
-    screen.fill(white)
+class StartScreen(Screen):
+    largeFont = pygame.font.Font('freesansbold.ttf', 80)
+    font = pygame.font.Font('freesansbold.ttf', 20)
     
-    ballRect.y += ystep
-    ballRect.x += xstep
-    print('ball',ballRect,xstep,ystep)
+    textBreakout = largeFont.render('BREAKOUT',True,teal,black)
+    textBreakoutX=screenWidth/2-textBreakout.get_width()/2
+    textBreakoutY=screenHeight/3
 
-    i = len(blocks)-1
-    while i >= 0:
-        rect = ballRect.rectintersection(blocks[i])
-        if rect.width>0 or rect.height > 0:
-            print('block collision',ballRect,blocks[i],rect)
-            blocks.pop(i)
-            if rect.width > rect.height:
-                ystep = -ystep
-            else:
-                xstep = -xstep
-        else:
-            pygame.draw.rect(screen,red,blocks[i].toPygame())
-        i -=1
-        
-    if paddleRect.colliderect(ballRect):
-        ystep = -ystep
-        ballMidPoint = ballRect.x+ballSize/2
-        paddleMidPoint = paddleRect.x+paddleRectWidth/2
-        xstep += (ballMidPoint - paddleMidPoint)/stepStrength
-        print('paddle collision',xstep,ystep)
-        
-    if ballRect.y + ballSize >  screenHeight:
-            running = False
-            pygame.quit()
-            sys.exit()
+    textStart=font.render('Press SPACE to start',True,teal,black)
+    textStartX=screenWidth/2-textStart.get_width()/2
+    textStartY=screenHeight/2-textStart.get_height()/2
+
+    def render(self):
+            surface.fill(black)
+
+            surface.blit(self.textBreakout,(self.textBreakoutX,self.textBreakoutY))
+            surface.blit(self.textStart,(self.textStartX,self.textStartY))
             
-    if ballRect.y < 0:
-        ballRect.y = 0
-        ystep = -ystep
-        print('top wall',ballRect, ystep)
+            pygame.display.update()
+            return True
+
+    def handleEvent(self,event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                screens.append(GameScreen())
+        return True
+
+class EndScreen(Screen):
+
+    font = pygame.font.Font('freesansbold.ttf', 20)
+    
+    def render(self):
+        surface.fill(black)
+        textGameover= self.font.render('GAMEOVER!', True, red, black)
+        textGameoverX=screenWidth/2-textGameover.get_width()/2
+        textGameoverY=screenHeight/2-textGameover.get_height()/2
+        surface.blit(textGameover,(textGameoverX,textGameoverY))
+        pygame.display.update()
+
+    def handleEvent(self,event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                screens.pop()
+        return True
         
-    if ballRect.x< 0:
-        ballRect.x = 0
-        xstep = -xstep
-        print('left wall',ballRect, xstep)
+class GameScreen(Screen):
+    
+    font = pygame.font.Font('freesansbold.ttf', 20)
+    largeFont = pygame.font.Font('freesansbold.ttf', 60)
+
+    paddleRectWidth = screenWidth/10
+    paddleRect = Rect(screenWidth*0.5-paddleRectWidth*0.5,screenHeight*0.9,paddleRectWidth,screenHeight/75)
+    paddleMoveSpeed = paddleRectWidth/4
+
+    ballSize = 20
+
+    ystep = 3
+    xstep = 0
+    stepStrength = 20.0
+ 
+    points = 0
+
+    def __init__(self):    
+        self.ballRect = Rect((screenWidth-self.ballSize)/2,screenHeight/2,self.ballSize,self.ballSize)
+
+        self.blocks = []
+        i = 30
+        while i<screenWidth-30:
+            self.blocks.append(Rect(i,80,20,20))
+            i += 50
+
+    def render(self):
+
+        surface.fill(black)
+
+        i = len(self.blocks)-1
+        if i == -1:
+            screens.pop()
+            screens.push(EndScreen())
+            return True
+    
+        self.ballRect.y += self.ystep
+        self.ballRect.x += self.xstep
         
-    if ballRect.x + ballSize>screenWidth:
-        ballRect.x=screenWidth-ballRect.width
-        xstep = -xstep
-        print('right wall',ballRect, xstep)
+        while i >= 0:
+            rect = self.ballRect.rectintersection(self.blocks[i])
+            if rect.width>0 or rect.height > 0:
+                self.blocks.pop(i)
+                self.points+=10
+                if rect.width > rect.height:
+                    self.ystep = -self.ystep
+                else:
+                    self.xstep = -self.xstep
+            else:
+                pygame.draw.rect(surface,red,self.blocks[i].toPygame())
+            i -=1
 
-    pygame.draw.rect(screen,black,paddleRect.toPygame())
-    pygame.draw.rect(screen,black,ballRect.toPygame())
+        textScore = self.font.render('Score: '+str(self.points)+' ', True, black, white)
+        textScoreX=screenWidth-textScore.get_width()
+        textScoreY=textScore.get_height()/3
+        surface.blit(textScore,(textScoreX,textScoreY))
+        
+        if self.paddleRect.colliderect(self.ballRect):
+            self.ystep = -self.ystep
+            ballMidPoint = self.ballRect.x+self.ballSize/2
+            paddleMidPoint = self.paddleRect.x+self.paddleRectWidth/2
+            self.xstep += (ballMidPoint - paddleMidPoint)/self.stepStrength
+            
+        if self.ballRect.y + self.ballSize >  screenHeight:
+                screens.pop()
+                return False
+                
+        if self.ballRect.y < 0:
+            self.ballRect.y = 0
+            self.ystep = -self.ystep
+            
+        if self.ballRect.x< 0:
+            self.ballRect.x = 0
+            self.xstep = -self.xstep
+            
+        if self.ballRect.x + self.ballSize>screenWidth:
+            self.ballRect.x=screenWidth-self.ballRect.width
+            self.xstep = -self.xstep
 
-    pygame.display.update()
+        pygame.draw.rect(surface,teal,self.paddleRect.toPygame())
+        pygame.draw.rect(surface,green,self.ballRect.toPygame())
 
-running = True
+        pygame.display.update()
+        return True
+
+    def handleEvent(self,event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                        self.paddleRect.x=min(self.paddleRect.x + self.paddleMoveSpeed,screenWidth - self.paddleRectWidth)
+            if event.key == pygame.K_LEFT:
+                        self.paddleRect.x=max(self.paddleRect.x - self.paddleMoveSpeed, 0)
+        elif event.type == pygame.MOUSEMOTION:
+            self.paddleRect.x = min(max(pygame.mouse.get_pos()[0] - self.paddleRectWidth*0.5, 0),
+                               screenWidth-self.paddleRectWidth)
+        return True
+
 pygame.key.set_repeat(50, 0)
-while running:
+screens.append(StartScreen())
+
+while True:
+    if not screens:
+        break
+    screen = screens[-1]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                sys.exit()
+                break
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                running = False
-                pygame.quit()
-                sys.exit()
-            if event.key == pygame.K_RIGHT:
-                    paddleRect.x=min(paddleRect.x + paddleMoveSpeed,screenWidth - paddleRectWidth)
-            if event.key == pygame.K_LEFT:
-                    paddleRect.x=max(paddleRect.x - paddleMoveSpeed, 0)
-        elif event.type == pygame.MOUSEMOTION:
-            paddleRect.x = min(max(pygame.mouse.get_pos()[0] - paddleRectWidth*0.5, 0),screenWidth-paddleRectWidth)
+                break
+                
+        screen.handleEvent(event)
+        
+    screen.render()
 
-    render()
+pygame.quit()
+sys.exit()
